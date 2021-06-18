23bbe988acfae1819dcd372ac4a2058a41696252
Pedro Colletti <pedrocolletti@icloud.com>
2021-06-18 12:33:44 -0300

    ImplementandoStyles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import avatar from './assets/avatar.jpg';
-import './App.css';
+import { Wrapper, WrapperName, ProfessionalExperiences, Certificates, Item, Info, Edit, WrapperSocialPages, WrapperCertificate, WrapperProfessional, WrapperSchool, WrapperContent, WrapperInfo, ContactList, BreackLine, Bold, Title2, Title6, LinkUrl, ProfilePicture } from './styles';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMailBulk, faMapMarkedAlt, faMobileAlt } from '@fortawesome/free-solid-svg-icons'
@@ -7,46 +7,46 @@ import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
-    <div className="App">
-            <div className="nome">
-              <h2>Pedro Colletti</h2>
-              <img src={avatar} className="foto-perfil"/>
-            </div>
-            <div className="info">
-              <h2 className="info-pessoais">Informações Pessoais</h2>
-                <ul>
-                  <label> <FontAwesomeIcon icon={ faMobileAlt }/>  (16) 99776-5777 </label><br></br>
-                  <label> <FontAwesomeIcon icon={ faMailBulk }/> pedrocolletti@icloud.com </label><br></br>
-                  <label> <FontAwesomeIcon icon={ faMapMarkedAlt }/> Av. Estrada de ferro, 148 - Araraquara-SP </label>
-                </ul>
-            </div>
-        <div className="conteudo">
-            <div className="escolaridade">
-              <h2>Escolaridade</h2>
-                <label><b>Anhanguera</b> Análise e Desenvolvimento de Sistemas </label><br></br>
-                <label><b>Unipampa</b> Relações Internacionais</label>
-            </div>
-            <div className="exp-profissional">
-              <h2>Experiência Profissional</h2>
-                <ul>
-                  <li><b>BeOnUp: </b>Estagiário Dev fzendo atribuições básicas para aprender tudo sobre desenvolvimento ou pelo menos grande parte das coisas, com o nosso querido Edi. Agora estou escrevendo isso pra encher linguiça pra ver o comportamento da página quanto a quebra de linhas.</li><br></br>
-                  <li><b>Saffi Consultoria: </b> Lá eu fazia um trabalho de vendas e não gostava. Tinha que ficar ligando e marcando reunião com empresarios, levava "tirada" toda hora e era bem estressante. </li>
-                </ul>
-            </div>
-            <div className="certificados">
-              <h2>Certificados</h2>
-                <ol>
-                  <li><b>HTML e CSS</b> -  Alura</li><br></br>
-                  <li><b>Básico em Lógica e programação</b> - DIO</li>
-                </ol>
-            </div>
-        </div>
-            <div className="paginas-sociais">
-              <h6><b>Páginas Sociais</b></h6>
-                  <a href="https://www.linkedin.com/in/pedrocolletti/" title="Linkedln">   <FontAwesomeIcon icon={ faLinkedin }/></a><br></br>
-                  <a href="https://github.com/PedroColletti" title="Github">  <FontAwesomeIcon icon={ faGithubSquare }/> <span class="menu-title sr-only">Github</span></a>
-            </div>
-    </div>
+    <Wrapper>
+            <WrapperName>
+              <Title2>Pedro Colletti</Title2>
+              <ProfilePicture src={avatar}/>
+            </WrapperName>
+            <WrapperInfo>
+              <Title2><Edit>Informações Pessoais</Edit></Title2>
+                <ContactList>
+                  <Info> <FontAwesomeIcon icon={ faMobileAlt }/>  (16) 99776-5777 </Info><BreackLine></BreackLine>
+                  <Info> <FontAwesomeIcon icon={ faMailBulk }/> pedrocolletti@icloud.com </Info><BreackLine></BreackLine>
+                  <Info> <FontAwesomeIcon icon={ faMapMarkedAlt }/> Av. Estrada de ferro, 148 - Araraquara-SP </Info>
+                </ContactList>
+            </WrapperInfo>
+      <WrapperContent>
+            <WrapperSchool>
+              <Title2>Escolaridade</Title2>
+                <Info><Bold>Anhanguera</Bold> Análise e Desenvolvimento de Sistemas </Info><BreackLine></BreackLine>
+                <Info><Bold>Unipampa</Bold> Relações Internacionais</Info>
+            </WrapperSchool>
+            <WrapperProfessional>
+              <Title2>Experiência Profissional</Title2>
+                <ProfessionalExperiences>
+                  <Item><Bold>BeOnUp: </Bold>Estagiário Dev fzendo atribuições básicas para aprender tudo sobre desenvolvimento ou pelo menos grande parte das coisas, com o nosso querido Edi. Agora estou escrevendo isso pra encher linguiça pra ver o comportamento da página quanto a quebra de linhas.</Item><BreackLine></BreackLine>
+                  <Item><Bold>Saffi Consultoria: </Bold> Lá eu fazia um trabalho de vendas e não gostava. Tinha que ficar ligando e marcando reunião com empresarios, levava "tirada" toda hora e era bem estressante. </Item>
+                </ProfessionalExperiences>
+            </WrapperProfessional>
+            <WrapperCertificate>
+              <Title2>Certificados</Title2>
+                <Certificates>
+                  <Item><Bold>HTML e CSS</Bold> -  Alura</Item><BreackLine></BreackLine>
+                  <Item><Bold>Básico em Lógica e programação</Bold> - DIO</Item>
+                </Certificates>
+            </WrapperCertificate>
+      </WrapperContent>
+            <WrapperSocialPages>
+              <Title6><Bold>Páginas Sociais</Bold></Title6>
+                  <LinkUrl href="https://www.linkedin.com/in/pedrocolletti/" title="Linkedln">   <FontAwesomeIcon color="black" icon={ faLinkedin }/></LinkUrl><BreackLine></BreackLine>
+                  <LinkUrl href="https://github.com/PedroColletti" title="Github">  <FontAwesomeIcon color="black"  icon={ faGithubSquare }/></LinkUrl>
+            </WrapperSocialPages>
+    </Wrapper>
   );
 }
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+
+export const Wrapper = styled.div`
+  text-align: center;
+  border: 15px solid black;
+`;
+export const WrapperName = styled.div`
+    padding: 50px;
+`;
+export const ContactList = styled.ul``;
+export const WrapperInfo = styled.div``;
+export const Edit = styled.b`
+    text-decoration-line:underline;
+`;
+export const Bold = styled.b`
+    color: black;
+`;
+export const Title2 = styled.h2`
+    color: black;
+    font-size: 35px;
+    
+`;
+export const Title6 = styled.h6`
+    color: black;
+    font-size: 20px;
+    padding-top: 20px;
+`;
+export const LinkUrl = styled.a``;
+export const ProfilePicture = styled.img`
+  border-radius: 100%;
+  max-width:  200px;
+  border: 2px solid black;
+  display: inline-block;
+  vertical-align: top;
+`;
+export const BreackLine = styled.br``;
+export const WrapperContent = styled.div`
+    text-align: left;
+    padding: 10px;
+    height: 100%;
+    width: 100%;
+    margin: 50px;
+`;
+export const WrapperSchool = styled.div``;
+export const WrapperProfessional = styled.div`
+  padding: 30px;
+  color: hsl(0, 0%, 37%);
+`;
+export const WrapperCertificate = styled.div`
+  text-align: left;
+`;
+export const WrapperSocialPages = styled.div`
+    padding-bottom: 20px;
+`;
+export const Info = styled.label``;
+export const Certificates = styled.ol``;
+export const ProfessionalExperiences = styled.ul``;
+export const Item = styled.li``;
+
+
+
